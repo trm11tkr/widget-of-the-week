054a9e19855ec215546d6b6e86f49d80acbb58c5
森田陸斗 <mrmrt1121@gmail.com>
2022-09-24 23:17:53 +0900

    OpacityPageを追加

--- lib/pages/opacity_page.dart
+++ lib/pages/opacity_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:widget_of_the_week/widgets/app_image_object.dart';
+import 'package:widget_of_the_week/widgets/app_scaffold.dart';
 
 class OpacityPage extends StatelessWidget {
   const OpacityPage({super.key});
@@ -7,6 +9,97 @@ class OpacityPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return AppScaffold(
+      title: 'Opacity',
+      child: Center(
+        child: Wrap(
+          direction: MediaQuery.of(context).orientation == Orientation.landscape
+              ? Axis.vertical
+              : Axis.horizontal,
+          children: const [
+            _LabelTile(opacity: 1),
+            _LabelTile(opacity: 0.8),
+            _LabelTile(opacity: 0.6),
+            _LabelTile(opacity: 0.4),
+            _LabelTile(opacity: 0.2),
+            _LabelTile(opacity: 0),
+            _WithBackGroundLabelTile(opacity: 1),
+            _WithBackGroundLabelTile(opacity: 0.8),
+            _WithBackGroundLabelTile(opacity: 0.6),
+            _WithBackGroundLabelTile(opacity: 0.4),
+            _WithBackGroundLabelTile(opacity: 0.2),
+            _WithBackGroundLabelTile(opacity: 0),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _LabelTile extends StatelessWidget {
+  const _LabelTile({required this.opacity});
+
+  final double opacity;
+
+  @override
+  Widget build(BuildContext context) {
+    return Wrap(
+      direction: Axis.vertical,
+      crossAxisAlignment: WrapCrossAlignment.center,
+      children: [
+        Opacity(
+          opacity: opacity,
+          child: const AppImageObject(),
+        ),
+        Text('Opacity: $opacity'),
+      ],
+    );
+  }
+}
+
+class _WithBackGroundLabelTile extends StatelessWidget {
+  const _WithBackGroundLabelTile({required this.opacity});
+
+  final double opacity;
+
+  @override
+  Widget build(BuildContext context) {
+    return Wrap(
+      direction: Axis.vertical,
+      crossAxisAlignment: WrapCrossAlignment.center,
+      children: [
+        Stack(
+          children: [
+            const AppImageObject(),
+            Opacity(
+              opacity: opacity,
+              child: _MyGradientWidget(opacity: opacity),
+            ),
+          ],
+        ),
+        Text('Opacity: $opacity'),
+      ],
+    );
+  }
+}
+
+class _MyGradientWidget extends StatelessWidget {
+  const _MyGradientWidget({required this.opacity});
+
+  final double opacity;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 120,
+      width: 120,
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+          colors: [Colors.blue, Colors.yellow],
+        ),
+      ),
+    );
   }
 }
