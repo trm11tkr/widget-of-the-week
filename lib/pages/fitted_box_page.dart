7e7175c462013d16798fa8d4190f2b4560e79c8b
森田陸斗 <mrmrt1121@gmail.com>
2022-10-01 21:03:04 +0900

    仮ページの作成

---
+++ lib/pages/fitted_box_page.dart
@@ -0,0 +1,17 @@
+
+import 'package:flutter/material.dart';
+import 'package:widget_of_the_week/widgets/app_scaffold.dart';
+
+class FittedBoxPage extends StatelessWidget {
+  const FittedBoxPage({super.key});
+
+  static const routeName = 'FittedBox';
+
+  @override
+  Widget build(BuildContext context) {
+    return AppScaffold(
+      title: 'FittedBox',
+      child: Container(),
+    );
+  }
+}
